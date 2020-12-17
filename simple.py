90445bb4d3045199c1a2d2c87ad342aefcdcbbf2
RK <rk@hpcnt.com>
2020-12-17 11:58:41 +0900

    Use function in health api

--- simple.py
+++ simple.py
@@ -1,4 +1,6 @@
 import os
+import logging
+import random
 
 import click
 from datetime import datetime
@@ -11,6 +13,7 @@ load_dotenv(verbose=True)
 
 app = Flask(__name__)
 
+
 @app.route('/ok')
 def hi():
     return 'OK'
@@ -18,14 +21,31 @@ def hi():
 
 @app.route('/', methods=['GET'])
 def check_health():
+    error = request.args.get('e')
+
+    raise_error_by_params(error)
+
+    data = gatter_basic_data()
+    content = '<br>'.join('%s : %s' % (key, value) for key, value in data.items())
+    return content
+
+
+def raise_error_by_params(error):
+    if error is not None:
+        error = 1.0 if error == '' else int(error) * 0.01
+        if error > random.random():
+            raise Exception('Some Exception')
+
+def gatter_basic_data():
     app_name = os.getenv('appname')
     fmt = '%Y-%m-%d %H:%M:%S'
     now = datetime.now().strftime(fmt)
     client_host = request.remote_addr
-    data = {'app': app_name, 'message': 'OK', 'now': now, 'ip': client_host}
-    content = '<br>'.join('%s : %s' % (key, value) for key, value in data.items())
-    return content
-
+    data = {'app': app_name,
+            'message': 'OK',
+            'now': now,
+            'ip': client_host}
+    return data
 
 @click.command()
 @click.option('--host', default='127.0.0.1')
